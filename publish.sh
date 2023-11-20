2489af993c654b08bbdfc3946b2d56e2405027c8
Sune <sune.jakobsson@gmail.com>
2023-11-20 15:59:34 +0100

    ma. 20. nov. 15:59:34 +0100 2023

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # script to update repository.
+cp /mnt/SSD_disk1/electric_today.json .
+cp /mnt/SSD_disk1/today.png .
+cp /mnt/SSD_disk1/tomorrow.png .
+#
+# push the new files to github
+#
 git pull
 git add --all
 git commit -m "$(date)"
