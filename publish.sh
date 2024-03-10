e5b1bb3390a509a5ab8d641bb19420beaddb9fb2
Sune <sune.jakobsson@gmail.com>
2024-03-10 15:00:03 +0100

    New plots sø. 10. mars 15:00:03 +0100 2024

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script to update repository.
 cp /mnt/SSD_disk1/electric_today.json .
-cp /mnt/SSD_disk1/today.png .
-cp /mnt/SSD_disk1/tomorrow.png .
+cp /mnt/SSD_disk1/today_Trondheim.png .
+cp /mnt/SSD_disk1/tomorrow_Trondheim.png .
 #
 # push the new files to github
 #
