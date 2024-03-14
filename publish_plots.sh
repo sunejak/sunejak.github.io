1b1ad5eaa40ea2753f2adbc428c3000702663686
Sune <sune.jakobsson@gmail.com>
2024-03-14 09:31:27 +0100

    New plots to. 14. mars 09:31:27 +0100 2024

--- publish_plots.sh
+++ publish_plots.sh
@@ -2,6 +2,8 @@
 # script to update repository.
 cp /mnt/SSD_disk1/today_Trondheim.png .
 cp /mnt/SSD_disk1/tomorrow_Trondheim.png .
+cp /mnt/SSD_disk1/today_Finland.png .
+cp /mnt/SSD_disk1/tomorrow_Finland.png .
 #
 # push the new files to github
 #
