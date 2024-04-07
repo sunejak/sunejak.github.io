4317093fa413b4647c797c811d0f9416addd7197
Sune <sune.jakobsson@gmail.com>
2024-04-07 15:00:03 +0200

    New plots sø. 07. april 15:00:03 +0200 2024

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sunejak.github.io
 My demo page on github.io
+
+Showing plots and prices for Trondheim and Finland
--- publish_price.sh
+++ publish_price.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # script to update repository.
 cp /mnt/SSD_disk1/electric_today.json .
+grep $(date -I) /mnt/SSD_disk1/electric.json | grep Finland > electric_Finland.json
 #
 # push the new files to github
 #
