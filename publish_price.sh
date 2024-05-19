a4a3df87cb066c303d499cedf073030634570563
Sune <sune.jakobsson@gmail.com>
2024-05-19 23:04:02 +0200

    New push of all sø. 19. mai 23:04:02 +0200 2024

--- publish_price.sh
+++ publish_price.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # script to update repository.
-cp /mnt/SSD_disk1/electric_today.json .
-grep $(date -I) /mnt/SSD_disk1/electric.json | grep Finland > electric_Finland.json
+# cp /mnt/SSD_disk1/electric_today.json .
+#
+grep $(date -I) /mnt/SSD_disk1/electric.json | grep Trondheim | tail -1 > electric_Trondheim.json
+grep $(date -I) /mnt/SSD_disk1/electric.json | grep Finland | tail -1 > electric_Finland.json
 #
 # push the new files to github
 #
